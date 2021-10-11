fea13c14f4aee428537c9d2aae87134d31348f7d
Rerebla <52045338+Rerebla@users.noreply.github.com>
2021-10-11 20:18:05 +0200

    Add bubble sort in Javascript

---
+++ bubbleSort.js
@@ -0,0 +1,12 @@
+function bubbleSort(array) {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      if (array[j] > array[j + 1]) {
+        const temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
+    }
+  }
+  return array;
+}
